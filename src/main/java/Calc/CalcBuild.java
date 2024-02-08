63e2ad204a22b7077736892198d4e3570edcf59f
Sailesh-Patel1 <Sailesh_patel_bsc@hotmail.co.uk>
2024-02-08 11:18:58 +0000

    Initial commit with .gitignore

---
+++ src/main/java/Calc/CalcBuild.java
@@ -0,0 +1,24 @@
+package Calc;
+
+public class CalcBuild {
+
+	public int add(int a, int b) {
+		return (a + b);
+	}
+
+	public double addDouble(double a, double b) {
+		return (a + b);
+	}
+
+	public int minus(int a, int b) {
+		return (a - b);
+	}
+
+	public int multiply(int a, int b) {
+		return (a * b);
+	}
+
+	public int divide(int a, int b) {
+		return (a / b);
+	}
+}
